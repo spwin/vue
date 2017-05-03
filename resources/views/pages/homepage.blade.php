8942b269b70e90ff94c50d4e711b1547ca232fab
Stanislav <stanislav.markevic@gmail.com>
2017-05-03 16:16:48 +0100

    switched to html display

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -2,10 +2,40 @@
 @section('app-page', 'homepage')
 
 @section('content')
-<about-section></about-section>
-            <summary-section></summary-section>
+<section class="bg-shadow text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <h1 class="section-heading">Stanislav Markevic</h1>
+                            <h2 class="smaller">Emergency WEB Developer</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <h3>Summary:</h3>
+                            <p>Working as a Freelancer for the last 6 years. Since 2012 helped lots of projects and solved hundreds of web application problems. Good at creating and simplifying complex algorithms, performing urgent fixes, setting various automation processes and reading someone's else code. <br/>
+                            Top priorities: <span class="my-bold">quality and liability.</span></p>
+                            <p>Looking forward for new challenges and opportunities.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <contacts-section :email="{{ $email }}"></contacts-section>
             <skills-section></skills-section>
-            <expertise-section></expertise-section>
+            <section class="mt-20px">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2 text-center expertise small">
+                            <p>
+                                Remote work | Custom PHP solutions | Performance upgrade | Urgent website fixes | Website maintenance | Third party integrations | Payment integration | Responsive websites | PHP Frameworks | Onsite SEO | WordPress | Linux
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <footer-section></footer-section>
 @endsection
